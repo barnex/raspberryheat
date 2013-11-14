f4ee4ae19a81a53030269c3385bf53a60703c2e8
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-11-14 21:31:59 +0100

    plot all sensors

--- Makefile
+++ Makefile
@@ -1,3 +1,2 @@
 all:
 	go build
-	go fmt
--- gnuplot.go
+++ gnuplot.go
@@ -9,22 +9,31 @@ import (
 )
 
 func servePlot(w http.ResponseWriter, r *http.Request) {
-	//url := r.URL.Path[len("/plot/"):]
-
 	now := time.Now()
 	todayLog := logFileName(now)
 	yesterLog := logFileName(now.Add(-24 * time.Hour))
 
-	cmd := fmt.Sprintf(`set key off; set xdata time; set timefmt "%%s"; set format x "%%H"; set term svg size 600,320 fsize 10; plot "%v" u 1:2 w li, "%v" u 1:2 w li; set output;exit;`, yesterLog, todayLog)
+	cmd := `set xdata time; set timefmt "%s"; set format x "%H"; set term png size 600,320; plot`
+	for i, s := range sensor {
+		if i != 0 {
+			cmd += ","
+		}
+		using := i + 2
+		cmd += fmt.Sprintf(`"<cat %v %v" u 1:%v w li title "%v"`, yesterLog, todayLog, using, s.Description())
+	}
+	cmd += `; set output;exit;`
+
 	args := []string{"-e", cmd}
 	log.Println("gnuplot", args)
-	out, err := exec.Command("gnuplot", args...).CombinedOutput()
+	out, err := exec.Command("gnuplot", args...).Output()
 	if err != nil {
+		log.Println(err)
+		log.Println(string(out))
 		msg := string(out) + "\n" + err.Error()
 		http.Error(w, msg, 400)
 		return
 	} else {
-		w.Header().Set("Content-Type", "image/svg+xml")
+		//w.Header().Set("Content-Type", "image/png")
 		w.Write(out)
 	}
 }
